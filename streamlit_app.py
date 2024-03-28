debfba1bf80869944f0be5dddcff40aef85a240c
Hemant Gupta <35923032+hghemant@users.noreply.github.com>
2024-03-28 16:25:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ def get_api_response(user_input, conversation_history, model_choice, top_k):
     payload = {
         "input": user_input,
         "conversation_history": conversation_history,
-        "model": model_choice, 
-        "top_k": top_k  
+        "model": model_choice,  # Including the selected model in the payload
+        "top_k": top_k  # Including the number of chunks to be retrieved
     }
     headers = {"Content-Type": "application/json"}
 
@@ -16,7 +16,7 @@ def get_api_response(user_input, conversation_history, model_choice, top_k):
     try:
         response = requests.post(api_url, json=payload, headers=headers)
         decoded_response = response.json()
-        api_response = decoded_response.get('body', ' no response body')
+        api_response = decoded_response.get('body', 'no response body')
         return api_response
     except Exception as e:
         print(f"Error processing the response: {e}")
@@ -24,21 +24,22 @@ def get_api_response(user_input, conversation_history, model_choice, top_k):
 
 
 def chat_interface():
-    st.title("Chat Interface with History")
-
-
-   
+    st.sidebar.title("Settings")
     model_choice = st.sidebar.selectbox(
         "LLM Model:",
-        ["Cohere", "Claude 3.0 (Haiku)", "Claude 3.0 (Sonnet)"]
+        ["Cohere", "Claude 3.0 (Haiku)", "Claude 3.0 (Sonnet)"],
+        index=0  # Default to the first option
     )
 
 
-   
-    top_k = st.sidebar.selectbox(
+    top_k = st.sidebar.select_slider(
         "No. of chunks to be retrieved (top_k):",
-        [5, 8, 10]
+        options=[5, 8, 10],
+        value=5  # Default value
     )
+
+
+    st.title("Chat Interface with History")
 
 
     if 'conversation_history' not in st.session_state:
@@ -54,15 +55,14 @@ def chat_interface():
         st.experimental_rerun()
 
 
+    st.write("Conversation History:")
     for index, exchange in enumerate(st.session_state.conversation_history):
-        st.text_area(label="You: ", value=exchange['user'], height=75, disabled=True, key=f"user_{index}")
-        st.text_area(label="AI: ", value=exchange['bot'], height=150, disabled=True, key=f"bot_{index}")
+        st.text_area(label=f"You: {index+1}", value=exchange['user'], height=75, disabled=True, key=f"user_{index}")
+        st.text_area(label=f"AI: {index+1}", value=exchange['bot'], height=150, disabled=True, key=f"bot_{index}")
 
 
 if __name__ == "__main__":
     chat_interface()
-
-
 
 
 
